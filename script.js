9c88dcd2ee37d012f8431fe2f4eb3012f9cdb13c
JessicaPreuss <82731575+JessicaPreuss@users.noreply.github.com>
2021-06-09 21:51:52 -0400

    First

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("This is Conditions Exercise 1")
+// let actualTemp = 68, desiredTemp = 70;
+
+// if (actualTemp > desiredTemp) {
+// console.log("Run A/C");
+// } else if (actualTemp < desiredTemp) {
+//     console.log ("Run Heat");
+// } else {
+//     console.log ("Standby");
+// }
+console.log("This is Conditions Exercise 2")
+
+let name1= "Grace", name2= "Richard", name3="Samaria";
+
